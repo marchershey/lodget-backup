642f0af23e84dde3ac7e53a4babad6b1e46a85fb
Marc Hershey <marchershey@users.noreply.github.com>
2022-02-16 23:51:35 -0500

    laravel test

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        './vendor/usernotnull/tall-toasts/config/**/*.php',
+        './vendor/usernotnull/tall-toasts/resources/views/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            // colors: {
+            //     gray: colors.gray,
+            //     muted: colors.gray[400],
+            //     primary: {
+            //         DEFAULT: colors.blue[600],
+            //         lighter: colors.blue[500],
+            //         lightest: colors.blue[300],
+            //         darker: colors.blue[800],
+            //         hover: colors.blue[700],
+            //         muted: colors.blue[300],
+            //     },
+            // },
+            // fontFamily: {
+            //     sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            // },
+            // lineClamp: {
+            //     7: '7',
+            //     8: '8',
+            //     9: '9',
+            //     10: '10',
+            // }
+        },
+    },
+
+    plugins: [
+        // require('@tailwindcss/forms'),
+        // require('@tailwindcss/aspect-ratio'),
+        // require('@tailwindcss/line-clamp'),
+    ],
+
+}
