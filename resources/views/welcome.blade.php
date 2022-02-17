f4eefd85d999f1258986485234f549451cea1614
Marc Hershey <marchershey@users.noreply.github.com>
2022-02-17 00:18:23 -0500

    fix: page title not set

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name')}}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}?{{ rand() }}">
     <script src="{{ asset('js/app.js') }}?{{ rand() }}"></script>
 </head>
